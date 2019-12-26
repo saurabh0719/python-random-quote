b6f4da31421c857f234d9ae875820090ecce1acf
saurabh0719 <saurabhpuj99@gmail.com>
2019-12-26 11:09:11 +0530

    random generator working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
+
 def primary():
-  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = len(quotes) -1
+  rnd = random.randint(0,last)
+
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
